3a7a3cffa97995c249fc309a6ccd45d7a60f334e
Faisal Shahzad <faisal.shahzad786@gmail.com>
2024-10-12 23:21:43 +0500

    Created custom hook to fetch the games from API

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,18 +1,11 @@
 import React, {useState, useEffect} from 'react'
-import apiClient from '../services/api-client.jsx'
 import { Text } from '@chakra-ui/react'
-
+import useGames from '../hooks/useGames.jsx'
 
 const GameGrid = () => {
-    const [games, setGames] = useState([]);
-    const [error, setError] = useState('');
 
-    useEffect(() => {
-        apiClient.get('/games')
-        .then( res => setGames(res.data.results))
-        .catch( err => setError(err.message))
-    }, []);
-
+    const {games, error} = useGames();
+    
   return (
     <>
     <div>GameGrid</div>
