2d3cf15f72a9f0c46a06e751e3327216cb03c7f1
Faisal Shahzad <faisal.shahzad786@gmail.com>
2024-10-15 00:54:16 +0500

    Displayed Game Critic Score

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Card, Image, Text, CardBody, Heading } from '@chakra-ui/react';
+import { Card, Image, Text, CardBody, Heading, HStack } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList.jsx';
+import CriticScore from './CriticScore.jsx';
 
 const GameCard = ({game}) => {
 
@@ -9,7 +10,10 @@ const GameCard = ({game}) => {
         <Image src={game.background_image} alt='image'></Image>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
-            <PlatformIconList platforms={game.parent_platforms} />
+            <HStack justifyContent={'space-between'}>
+              <PlatformIconList platforms={game.parent_platforms} />
+              <CriticScore score={game.metacritic} />
+            </HStack>
         </CardBody>
     </Card>  
   )
