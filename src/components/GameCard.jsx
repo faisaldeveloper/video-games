85e736c1934880bb734e2c5eade7d47cedf13621
Faisal Shahzad <faisal.shahzad786@gmail.com>
2024-10-15 01:06:02 +0500

    Optimized Game images

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Card, Image, Text, CardBody, Heading, HStack } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList.jsx';
 import CriticScore from './CriticScore.jsx';
+import getCroppedImageUrl from '../services/image-url.jsx';
 
 const GameCard = ({game}) => {
 
   return (
     <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image} alt='image'></Image>
+        <Image src={ getCroppedImageUrl(game.background_image) } alt='image'></Image>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
             <HStack justifyContent={'space-between'}>
